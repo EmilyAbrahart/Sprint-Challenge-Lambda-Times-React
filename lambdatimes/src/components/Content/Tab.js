2c6857beb2d644915c3200b7794f15b96cc40013
EmilyAbrahart <emilyabrahart@gmail.com>
2019-06-07 11:06:57 +0100

    Fix issue with tab class name

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -29,13 +29,10 @@ const Tab = props => {
 	/* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-	const isActiveTab = () => {
-		return props.selectedTab === props.tab ? 'active-tab' : null;
-	};
 
 	return (
 		<TabDiv
-			className={`tab ${isActiveTab}`}
+			className={`tab ${props.selectedTab === props.tab ? 'active-tab' : ''}`}
 			onClick={() => {
 				/* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
